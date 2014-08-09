dc20743cad8e9b7c98924d265fb13f94826b4982
kishanov <kirill.ishanov@gmail.com>
2014-08-08 18:54:25 -0700

    [ui] initial layout for index page and web app setup

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
+
+import game_engine
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    boards = game_engine.get_all_boards()
+    return render_template('games_list.html', boards=boards)
+
+
+@app.route('/boards/<int:board_id>')
+def board_page(board_id):
+    return json.dumps({"board": game_engine.get_board(board_id),
+                       "id": board_id})
 
 
 if __name__ == '__main__':
